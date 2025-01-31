9093b8d7e874df349ad8ddfbfcfb51140e2dd575
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-01-31 14:54:05 +0530

    Create program8.py

---
+++ LAB2/program8.py
@@ -0,0 +1,7 @@
+# Input base and height of the triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+# Calculate area
+area = 0.5 * base * height
+# Display the result
+print(f"The area of the triangle is: {area}")
