ecf3032a9743d695424de018d12fa3ef4231317b
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-01-31 14:54:36 +0530

    program9.py

---
+++ LAB2/program9.py
@@ -0,0 +1,8 @@
+# Input two values
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+# Swap values
+a, b = b, a
+# Display swapped values
+print(f"The value of a after swapping is: {a}")
+print(f"The value of b after swapping is: {b}")
