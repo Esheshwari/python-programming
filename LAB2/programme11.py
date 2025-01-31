68e63677569d5effe6791da40b70593752ce4b76
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-01-31 15:01:16 +0530

    programme11.py

---
+++ LAB2/programme11.py
@@ -0,0 +1,14 @@
+a = 5  # 0101
+b = 6  # 0110
+# Bitwise AND
+print(a & b)  # 0100 -> 4
+# Bitwise OR
+print(a | b)  # 0111 -> 7
+# Bitwise XOR
+print(a ^ b)  # 0011 -> 3
+# Bitwise Left Shift
+print(a << 2)  # 010100 -> 20
+# Bitwise Right Shift
+print(a >> 2)  # 0001 -> 1
+# Bitwise NOT
+print(~a)  # 1010 -> -6
