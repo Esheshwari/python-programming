f5728f3dc78574ff9897574d57ded2a54fe6a495
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-01-31 15:02:00 +0530

    programme12.py

---
+++ LAB2/programme12.py
@@ -0,0 +1,8 @@
+# Left to right (Multiplication)
+result = 2 * 3 * 4  # (6 * 4) -> 24
+# # Right to left (Exponentiation)
+result = 2**3**2  # (2 ** (3 ** 2)) -> 512
+# # Precedence example
+result = 4 + 3 * 5  # (4 + (3 * 5)) -> 19
+result = 4 + 2**5 // 10  # 4 + (32 // 10) -> 4 + 3 -> 7
+result
