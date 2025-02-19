0aaa059830adc3519b527cfe6ffe04a2708df945
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-02-19 19:39:31 +0530

    tutorial1.py

---
+++ EXAM/tutorial1.py
@@ -0,0 +1,20 @@
+x=print("Hello")
+print(type(x))
+'''s={1,2,3}
+print(s[0])'''
+lst=[1,2,3]
+lst.extend([4,5])
+print(lst)
+tup=(1,2,3)
+print(tup[ :2])
+x=10
+if x==10:
+    print("A")
+elif x>5:
+    print("B")
+else:
+    print("C")
+lis=[1,2,3]
+tup_1=(1,2,3)
+lis[0]=99
+#tup_1[0]=99
