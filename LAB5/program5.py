45a842ad6a70fcefaa347b49499c50afdafcdc26
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-02-28 10:18:44 +0530

    program5.py

---
+++ LAB5/program5.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Use the correct function name 'linspace' to generate linearly spaced values
+array7 = np.linspace(0, 10, 11)#start, stop
+
+# Print the generated array
+print(array7)
