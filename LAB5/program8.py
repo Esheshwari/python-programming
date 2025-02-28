e77d34ca6410586f6377208a39a3db7f92c1cf59
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-02-28 10:51:14 +0530

    program8.py

---
+++ LAB5/program8.py
@@ -0,0 +1,7 @@
+import numpy as np  # Import numpy module as np
+
+# Generate a random array with 5 integers between 1 and 20 (exclusive of 20)
+random_array = np.random.randint(1, 20, 5)
+
+# Print the generated random array
+print(random_array)
